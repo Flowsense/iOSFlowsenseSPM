57b169d6518a8885917e84b6b3077935204fc86e
Feller <feller.ailton@edu.univali.br>
2022-02-16 15:16:15 -0300

    Update the SDK for version: None

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "iOSFlowsenseSPM",
+    platforms: [.iOS(.v11)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "iOSFlowsenseSPM",
+            targets: ["iOSFlowsenseSPMTargets"]),
+    ],
+    dependencies: [
+        .package(name:"AWSiOSSDKV2", url: "https://github.com/aws-amplify/aws-sdk-ios-spm.git", branch: "main")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "FlowsenseSDK",
+            url: "https://github.com/Flowsense/SDKiOS-SP/releases/download/None/SDK_None.zip",
+            checksum: "52573ac512bc87cda4b3d9a3139108df32ac4eecc1a6fd4d4d2d865644a8e6a8"
+        ),
+        .target(name: "iOSFlowsenseSPMTargets",
+                dependencies: [
+                    .target(name: "FlowsenseSDK"),
+                    .product(name: "AWSCore", package: "AWSiOSSDKV2"),
+                    .product(name: "AWSKinesis", package: "AWSiOSSDKV2")
+                ],
+                path: "Sources")
+    ]
+)
---
+++ Sources/iOSFlowsenseSPM/iOSFlowsenseSPM.swift
@@ -0,0 +1,6 @@
+public struct iOSFlowsenseSPM {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
